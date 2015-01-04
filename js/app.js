13069cd565d3154ad7a55e07e4b412dae6c4aa8d
Yavor Ivanov <davko996@gmail.com>
2015-01-04 22:30:30 +0200

    More routing added. Log in and Register functionality added. View all ads too.

---
+++ js/app.js
@@ -0,0 +1,54 @@
+'use-strict';
+
+(function(){
+    var adsApp = angular.module('adsApp', [
+        'ui.router',
+        'adsControllers'
+    ]);
+
+    adsApp.config(function($stateProvider) {
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                views: {
+                    "mainView": { templateUrl: "./partials/all-ads.html" },
+                    "left-side-view": { templateUrl: "./partials/left-side-panel.html" },
+                    "right-side-view": { templateUrl: "./partials/right-side-panel.html" },
+                    "header" : { templateUrl: "./partials/header.html"}
+                }
+            }).state('logIn', {
+                url: "/login",
+                views: {
+                    "mainView": { templateUrl: "./partials/login-view.html" },
+                    "left-side-view": { templateUrl: "./partials/left-side-panel.html" },
+                    "right-side-view": { templateUrl: "./partials/right-side-panel.html" },
+                    "header" : { templateUrl: "./partials/header.html"}
+                }
+            }).state('register', {
+                url: "/register",
+                views: {
+                    "mainView": { templateUrl: "./partials/register-view.html" },
+                    "left-side-view": { templateUrl: "./partials/left-side-panel.html" },
+                    "right-side-view": { templateUrl: "./partials/right-side-panel.html" },
+                    "header" : { templateUrl: "./partials/header.html"}
+                }
+            }).state('index', {
+                url: "",
+                views: {
+                    "mainView": { templateUrl: "./partials/all-ads.html" },
+                    "left-side-view": { templateUrl: "./partials/left-side-panel.html" },
+                    "right-side-view": { templateUrl: "./partials/right-side-panel.html" },
+                    "header" : { templateUrl: "./partials/header.html"}
+                }
+            })
+            .state('home-logged', {
+                url: "/home-logged",
+                views: {
+                    "mainView": { templateUrl: "./partials/all-ads.html" },
+                    "left-side-view": { templateUrl: "./partials/left-side-panel-loged.html" },
+                    "right-side-view": { templateUrl: "./partials/right-side-panel.html" },
+                    "header" : { templateUrl: "./partials/header-logged.html"}
+                }
+            })
+    });
+}());
---
+++ js/controllers.js
@@ -0,0 +1,115 @@
+'use-strict';
+
+(function(){
+
+    var adsControllers = angular.module('adsControllers', []);
+
+    adsControllers.controller('AdListCtrl', ['$http', function ($http) {
+        var self = this;
+
+        self.allAds = [];
+        changeTitle('Ads - Home');
+
+        $http.get(baseURL + "ads", {})
+            .success(function(data){
+                console.log(data);
+                self.allAds = data.ads;
+            })
+            .error(function(data){
+                console.error(data);
+            }
+        );
+    }]);
+
+    adsControllers.controller('LoggedHeaderCtrl', ['$http', function ($http) {
+        var self = this;
+        self.username = JSON.parse(localStorage.getItem('user-data')).username;
+
+        self.logOut = function(){
+            localStorage.removeItem('user-data');
+            reroute('#/home');
+        }
+
+    }]);
+
+    adsControllers.controller('LogInCtrl', ['$http', function($http){
+        changeTitle('Ads - Log in');
+        var self = this;
+        self.username = "";
+        self.password = "";
+
+        self.logIn = function(){
+            if (self.username == "" || self.password == ""){
+                alert('Passwords don\'t match. -' + self.password + "-");
+            } else {
+                $http.post(baseURL + '/user/Login', JSON.stringify({
+                        username: self.username,
+                        password: self.password
+                    })).success(function(data){
+                    localStorage.setItem('user-data', JSON.stringify(data));
+                    reroute('#/home-logged');
+                }).error(function(data){
+                    console.error(data);
+                });
+            }
+        }
+
+    }]);
+
+    adsControllers.controller('RegisterCtrl', ['$http', function($http){
+        changeTitle('Ads - Register');
+        var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        var ck_username = /^[A-Za-z0-9_]{1,20}$/;
+        var errors = [];
+
+        var self = this;
+        self.username = "";
+        self.password = "";
+        self.password2 = "";
+        self.email = "";
+        self.phoneNumber = "";
+        self.fullName = "";
+        self.town = "sofia";
+
+        self.register = function(){
+
+            if (self.username.trim() == ""){ errors.push('Username field can\'t be empty'); }
+            if (self.password.trim() == ""){ errors.push('Password field can\'t be empty'); }
+            if (self.email.trim() == ""){ errors.push('Email field can\'t be empty'); }
+            if (self.phoneNumber.trim() == ""){ errors.push('Phone number field can\'t be empty'); }
+            if (self.fullName.trim() == ""){ errors.push('Full name field can\'t be empty'); }
+
+            if (errors.length > 0){
+                alert(errors.join('\n'));
+                errors = [];
+                return;
+            }
+
+            if (!ck_email.test(self.email)) { errors.push('Invalid email.'); }
+            if (!ck_username.test(self.username)) { errors.push('Invalid username.'); }
+            if (self.password != self.password2) { errors.push('Passwords don\'t match.'); }
+
+            if (errors.length > 0){
+                alert(errors.join('\n'));
+                errors = [];
+                return;
+            }
+
+            $http.post(baseURL + "user/register", JSON.stringify({
+                    username: self.username,
+                    password: self.password,
+                    confirmPassword: self.password2,
+                    name: self.fullName,
+                    email: self.email,
+                    phone: self.phoneNumber,
+                    townId: 1
+                })).success(function(data){
+                console.log(data);
+            }).error(function(data){
+                console.error(data);
+            });
+        }
+
+    }]);
+}());
+
